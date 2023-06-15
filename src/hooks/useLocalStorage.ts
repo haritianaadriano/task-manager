04b019ca90806c268ff2615b4126400e02ebe38c
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-15 10:18:54 +0300

    refactor: useLocalStorage for uselly

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,12 @@
-const useLocalStorage = (value: any) => {
-  localStorage.setItem("task", value);
+import {Type} from "@/types/localStorageType";
+
+const useLocalStorage = (value: any, type: Type, key: string) => {
+  if(type === Type.GET){
+    localStorage.getItem(key);
+  }
+  else if(type === Type.PUT){
+    localStorage.setItem(key, value);
+  }
 }
 
 export {
